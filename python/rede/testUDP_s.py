827ad6295b35956ea652e603b1bd66758fecef5c
Felipe Desiglo Ferrare <desiglo84@gmail.com>
2025-02-28 17:24:54 -0300

    Add files via upload

---
+++ python/rede/testUDP_s.py
@@ -0,0 +1,9 @@
+import socket
+target_host = "127.0.0.1"
+target_port = 9997
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# send some data
+client.sendto(b"AAABBBCCC",(target_host,target_port))
+# receive some data
+client.close()
